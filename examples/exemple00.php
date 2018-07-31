fb57699c2c500326284bfe87f7f77dede6398d1c
kamito98 <39121874+kamito98@users.noreply.github.com>
2018-07-31 20:00:29 +0800

    change output file name to "example00.pdf" and add description for some code

--- examples/exemple00.php
+++ examples/exemple00.php
@@ -20,11 +20,11 @@ try {
     include dirname(__FILE__).'/res/exemple00.php';
     $content = ob_get_clean();
 
-    $html2pdf = new Html2Pdf('P', 'A4', 'fr');
-    $html2pdf->setDefaultFont('Arial');
-    $html2pdf->writeHTML($content);
-    $html2pdf->output('exemple00.pdf');
-} catch (Html2PdfException $e) {
+    $html2pdf = new Html2Pdf('P', 'A4', 'fr');      //create a new PDF document with potrait orientation, A4 format, and set French as default language
+    $html2pdf->setDefaultFont('Arial');             //set "Arial" as default font type
+    $html2pdf->writeHTML($content);                 
+    $html2pdf->output('example00.pdf');             //name the PDF document as 'example00.pdf' and send it to browser
+} catch (Html2PdfException $e) {                    
     $html2pdf->clean();
 
     $formatter = new ExceptionFormatter($e);
